be7314ecc584678be3c03ec5cb5d7170995480a5
whatisaProCoder <debnathpritam2005@gmail.com>
2026-02-13 14:10:47 +0000

    db layer done for components

--- db/queries.js
+++ db/queries.js
@@ -2,7 +2,7 @@ const pool = require("./pool")
 
 async function getAllCategories() {
   const { rows } = await pool.query("SELECT * FROM categories ORDER BY category ASC")
-  return rows;
+  return rows
 }
 
 async function addCategory({ categoryName }) {
@@ -22,10 +22,63 @@ async function deleteCategory({ id }) {
   await pool.query("DELETE FROM categories where id = $1", [id])
 }
 
+async function addComponent({ name, description, quantity, category_id }) {
+  const { rows } = await pool.query(
+    "INSERT INTO components (name, description, quantity, category_id) VALUES ($1, $2, $3, $4)",
+    [name, description, quantity, category_id]
+  )
+  return rows[0]
+}
+
+async function getComponent({ id }) {
+  const { rows } = await pool.query(
+    "SELECT co.id, co.name, co.description, co.quantity, ca.id as category_id, ca.category FROM components co join categories ca on co.category_id = ca.id where co.id = $1",
+    [id]
+  )
+  return rows[0]
+}
+
+async function getComponentsCategoryWise({ categoryID }) {
+  const { rows } = await pool.query("SELECT * FROM components WHERE category_id = $1 ORDER BY name ASC", [categoryID])
+  return rows
+}
+
+async function getAllComponents() {
+  const categories = await getAllCategories()
+
+  return Promise.all(
+    categories.map(async (category) => ({
+      category_id: category.id,
+      category: category.category,
+      components: await getComponentsCategoryWise({
+        categoryID: category.id
+      })
+    }))
+  )
+}
+
+async function updateComponent({ id, name, description, quantity, category_id }) {
+  await pool.query(
+    "UPDATE components SET name = $1, description = $2, quantity = $3, category_id = $4 where id = $5",
+    [name, description, quantity, category_id, id]
+  )
+}
+
+async function deleteComponent({ id }) {
+  await pool.query("DELETE FROM components where id = $1", [id])
+}
+
+
 module.exports = {
   getAllCategories,
   addCategory,
   getCategory,
   updateCategory,
-  deleteCategory
+  deleteCategory,
+  addComponent,
+  getComponent,
+  getAllComponents,
+  getComponentsCategoryWise,
+  updateComponent,
+  deleteComponent
 }
